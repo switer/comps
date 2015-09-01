aa0636e17345c9b38bd56b7923e183e9c209e497
switer <guankaishe@gmail.com>
2015-09-01 16:30:45 +0800

    attribute parser compelete

--- index.js
+++ index.js
@@ -1,52 +1,64 @@
 'use strict';
 
 var BlockNested = require('block-nested')
-// var SELF_CLOSE_REG = /\{%[\s\S]+?\/%\}/
-// var CLOSE_REG = /\{%\/[\s\S]+?%\}/
 
-// var parser = require('../index')(
-// 	/\{%[\s\S]+?%\}/g,
-// 	// first match self-close tag
-// 	// this judge condition is base on the condition of operator match
-// 	function isSelfCloseTag (c) {
-// 		return SELF_CLOSE_REG.test(c)
-// 	},
-// 	// secondary match block-open tag
-// 	// this judge condition is base on the condition of operator match
-// 	function isOpenTag (c) {
-// 		return !CLOSE_REG.test(c)
-// 	},
-// 	function handler(ast) {
-// 		switch (ast.type) {
-// 			case 'close':
-// 				return '<component />'
-// 			case 'block':
-// 				return '<component-block>'+ ast.content + '</component-block>'
-// 		}
-// 	}
-// )
-
-function regStr (str) {
-	return '\\' + str.split('').join('\\')
-}
-/\{%[\s\S]+?%\}/gm
-var _block_close_reg = new RegExp(regStr(_config.openTag) + '\/[\\s\\S]+?' + regStr(_config.closeTag()))
-var _operator_reg = new RegExp(regStr(_config.openTag))
-var _components = {}
+/**
+ * Internal variables
+ */
+var _blockComponents = {}
+var _selfCloseComponents = {}
+/**
+ * Comps's config
+ */
 var _config = {
 	openTag: '{%',
 	closeTag: '%}'
 }
-
+/**
+ * Private match regexps
+ */
+var _trim_reg = _genTrimReg()
+var _wildcard_reg = _genWildcardReg()
+var _block_close_reg = _genBlockCloseReg()
+var _self_close_reg = _genSelfCloseReg()
+/**
+ * Interal util methods
+ */
+function _genRegStr (str) {
+	return '\\' + str.split('').join('\\')
+}
+function _genBlockCloseReg () {
+	return new RegExp(_genRegStr(_config.openTag) + '\/[\\s\\S]+?' + _genRegStr(_config.closeTag()))
+}
+function _genSelfCloseReg () {
+	return new RegExp(_genRegStr(_config.openTag) + '\/[\\s\\S]+?/' + _genRegStr(_config.closeTag()))
+}
+function _genWildcardReg () {
+	return new RegExp(_genRegStr(_config.openTag) + '[\\s\\S]+?' + _genRegStr(_config.closeTag), 'gm')
+}
+function _genTrimReg () {
+	return new RegExp('^' + _genRegStr(_config.openTag) + '\\s|\\s' + _genRegStr(_config.closeTag) + '$', 'gm')
+}
+function _getNameFromAttribute(c) {
+	return c.replace(_trim_reg, '').split(' ')[0]
+}
+function _getAttributes(c) {
+	var attStr = c.replace(_trim_reg, '')
+	
+}
+/**
+ * Singleton parser instance
+ */
 var _Parser = BlockNested(
 	function operator() {
-		return _operator_reg
+		return _wildcard_reg
 	},
 	function isSelfClose (c) {
-		return 
+		var cname = _getNameFromAttribute(c)
+		return !!_selfCloseComponents[cname] || _self_close_reg.test(c)
 	},
 	function isOpen (c) {
-		return
+		return !_block_close_reg.test(c)
 	},
 	function handler(c) {
 		switch (ast.type) {
@@ -57,21 +69,27 @@ var _Parser = BlockNested(
 		}
 	}
 )
+/**
+ * Comps module interfaces
+ */
+function Comps (text) {
+}
+Comps.component = function (name, def) {
 
-function Comps () {
 }
 Comps.compile = function () {
 
 }
-Comps.render = function () {
-	
-}
 Comps.config = function (name, value) {
+	_config[name] = value
 	switch (name) {
 		case 'openTag':
 		case 'closeTag':
+			_block_close_reg = _genBlockCloseReg()
+			_self_close_reg = _genSelfCloseReg()
+			_wildcard_reg = _genWildcardReg()
+			_trim_reg = _genTrimReg()
+			break
 	}
-
-	_config[name] = value
 }
 module.exports = Comps
