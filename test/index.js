dbbe8dc06d767c761f1e4421106927f11be91059
switer <guankaishe@gmail.com>
2015-11-29 11:01:45 +0800

    add pagelet tag test case

--- test/index.js
+++ test/index.js
@@ -10,7 +10,7 @@ comps.componentLoader(function (name) {
 comps.componentTransform(function (name) {
     this.$attributes['r-component'] = 'c-' + name
 })
-describe('#Component', function () {
+describe('Class-Methods: Comps()', function () {
     it('Render component tag', function () {
         var r = comps({
             template: '<div>{% component $id="header" /%}</div>'
@@ -24,7 +24,7 @@ describe('#Component', function () {
         assert.equal(r, '<div><div class="header" data-index="{index: 0}" r-component="c-header"></div></div>')
     })
 })
-describe('#Config', function () {
+describe('Class-Methods: config()', function () {
     it('Custom tag', function () {
         comps.config('openTag', '<%')
         comps.config('closeTag', '%>')
@@ -32,5 +32,23 @@ describe('#Config', function () {
             template: '<div><% component $id="header" /%></div>'
         })
         assert.equal(r, '<div><div r-component="c-header"><div class="header"></div></div></div>')
+        comps.config('openTag', '{%')
+        comps.config('closeTag', '%}')
+    })
+})
+describe('Tags:pagelet', function () {
+    it('Render pagelet by id', function () {
+        var r = comps({
+            pagelet: 'header',
+            template: '<div>{% pagelet $id="header" %}<div class="header"></div>{%/pagelet%}</div>'
+        })
+        assert.equal(r, '<div data-pageletid="header"><div class="header"></div></div>')
+    })
+    it('Render nested pagelet', function () {
+        var r = comps({
+            pagelet: 'header.content',
+            template: '<div>{% pagelet $id="header" %}{% pagelet $id="content" %}<div class="header-content"></div>{%/pagelet%}{%/pagelet%}</div>'
+        })
+        assert.equal(r, '<div data-pageletid="header.content"><div class="header-content"></div></div>')
     })
 })
